2c9279dae9bf694c29aa47d82e9a76c502d5f9cc
Krishan Kumar Sharma <pradhankrishan1251@gmail.com>
2026-05-15 00:39:23 +0530

    add domain of vercel for cors

--- server.js
+++ server.js
@@ -27,6 +27,10 @@ connectDB();
 const Router = require('./src/routers/Router');
 app.use('/api/route', Router);
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the Socially Approved API');
+});
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
